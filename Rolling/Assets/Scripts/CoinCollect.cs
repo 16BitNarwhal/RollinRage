2ccff3344e649d20bebf2e99779f53a050e3487a
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2020-06-16 17:32:47 -0400

    ooooo

---
+++ Rolling/Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+    public GeneralManager generalManager;
+
+    void Start() {
+        transform.position = new Vector3(Random.Range(-5, 5),Random.Range(-1, 1),0) * 5;
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.tag == "Player") {
+            generalManager.score += 1;
+            Destroy(gameObject);
+        }
+    }
+}
